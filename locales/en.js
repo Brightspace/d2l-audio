f8a56ba0370dab638b6791049ac455c967fb3ab4
Evan Lemieux <Evan.Lemieux@D2L.com>
2019-10-30 23:47:14 -0500

    Prepping for serge

---
+++ locales/en.js
@@ -0,0 +1,14 @@
+/* eslint-disable quotes, indent */
+window.D2L = window.D2L || {};
+window.D2L.D2LAudio = window.D2L.D2LAudio || {};
+
+/**
+	* This file is auto-generated. Do not modify.
+	* @polymerBehavior window.D2L.D2LAudio.LangEnBehavior
+	*/
+window.D2L.PolymerBehaviors.D2LAudio.LangTerms["en"] = {
+				"Play": "Play",
+				"Pause": "Pause",
+				"AudioPlayer": "Audio Player",
+				"SeekBar": "Seek Bar"
+};
