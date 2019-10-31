624b9707b90b06c0133caf864fec1b32f993658d
Evan Lemieux <Evan.Lemieux@D2L.com>
2019-10-31 14:12:26 -0500

    Code review changes

--- locales/en.js
+++ locales/en.js
@@ -1,14 +1,16 @@
 /* eslint-disable quotes, indent */
 window.D2L = window.D2L || {};
-window.D2L.D2LAudio = window.D2L.D2LAudio || {};
+window.D2L.PolymerBehaviors = window.D2L.PolymerBehaviors || {};
+window.D2L.PolymerBehaviors.D2LAudio = window.D2L.PolymerBehaviors.D2LAudio || {};
+window.D2L.PolymerBehaviors.D2LAudio.LangTerms = window.D2L.PolymerBehaviors.D2LAudio.LangTerms || {};
 
 /**
 	* This file is auto-generated. Do not modify.
-	* @polymerBehavior window.D2L.D2LAudio.LangEnBehavior
+	* @polymerBehavior
 	*/
 window.D2L.PolymerBehaviors.D2LAudio.LangTerms["en"] = {
-				"Play": "Play",
-				"Pause": "Pause",
-				"AudioPlayer": "Audio Player",
-				"SeekBar": "Seek Bar"
+	"Play": "Play",
+	"Pause": "Pause",
+	"AudioPlayer": "Audio Player",
+	"SeekBar": "Seek Bar"
 };
