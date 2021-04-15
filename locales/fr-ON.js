2a2ceb61663128849465377b20a1825b065920a0
marcsoiferman <60416666+marcsoiferman@users.noreply.github.com>
2021-04-15 11:20:19 -0500

    Serge support (#37)
    * Serge support
    
    * Update d2l-audio.serge.json
    
    Co-authored-by: Danny Gleckler <daniel.gleckler@d2l.com>
    
    * Added new language packs
    
    Co-authored-by: Danny Gleckler <daniel.gleckler@d2l.com>

---
+++ locales/fr-ON.js
@@ -0,0 +1,16 @@
+/* eslint-disable quotes, indent */
+window.D2L = window.D2L || {};
+window.D2L.PolymerBehaviors = window.D2L.PolymerBehaviors || {};
+window.D2L.PolymerBehaviors.D2LAudio = window.D2L.PolymerBehaviors.D2LAudio || {};
+window.D2L.PolymerBehaviors.D2LAudio.LangTerms = window.D2L.PolymerBehaviors.D2LAudio.LangTerms || {};
+
+/**
+	* This file is auto-generated. Do not modify.
+	* @polymerBehavior
+	*/
+window.D2L.PolymerBehaviors.D2LAudio.LangTerms["fr-ON"] = {
+	"Play": "Play",
+	"Pause": "Pause",
+	"AudioPlayer": "Audio Player",
+	"SeekBar": "Seek Bar"
+};
