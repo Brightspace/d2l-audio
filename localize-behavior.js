deafc2c5856f59c33710e4c5dc5c4e6f288433fb
Marc Soiferman <Marc.Soiferman@D2L.com>
2021-04-15 10:54:51 -0500

    Added new language packs

--- localize-behavior.js
+++ localize-behavior.js
@@ -1,6 +1,19 @@
 import '@polymer/polymer/polymer-legacy.js';
 import 'd2l-localize-behavior/d2l-localize-behavior.js';
+import './locales/ar.js';
+import './locales/cy.js';
+import './locales/da.js';
+import './locales/de.js';
 import './locales/en.js';
+import './locales/es.js';
+import './locales/fr-ON.js';
+import './locales/fr.js';
+import './locales/ja.js';
+import './locales/ko.js';
+import './locales/nl.js';
+import './locales/pt.js';
+import './locales/sv.js';
+import './locales/tr.js';
 
 window.D2L = window.D2L || {};
 window.D2L.PolymerBehaviors = window.D2L.PolymerBehaviors || {};
